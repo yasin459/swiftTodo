0d54c3a42aaa1f81d8d3f39a5ab311debbc71aa5
yasin459 <yasinnmoosavi@gmail.com>
2021-06-06 22:57:59 +0430

    nearly body is completed

--- main.swift
+++ main.swift
@@ -13,6 +13,8 @@ public class Deck{
     public func addTodo(todo:Todo){
         self.todos.append(todo)
     }
+    
+    
 }
 public class Todo{
     public static var allTodos = [Todo]()
@@ -20,18 +22,44 @@ public class Todo{
     private var content:String = ""
     private var date:Date!
     private var priority:Int!
-    init(title:String,content:String, date:Date,priority:Int){
+    private var deck:Deck!
+    init(title:String,content:String, date:Date,priority:Int,deck:Deck){
         self.title = title
         self.content = content
         self.date = date
         self.priority = priority
+        self.deck = deck
     }
     public func getTitle()-> String{
         return self.title
     }
+    public func getDate()-> Date{
+        return self.date
+    }
+    public func getContent()-> String{
+        return self.content
+    }
+    public func getPriority()-> Int{
+        return self.priority
+    }
+    
+    public func deleteTodo(){
+        // to delete from deck and allTodos
+    }
+    public func editTodo(){
+        
+    }
 }
 func startMenu(){
-    let todo = Todo(title:"1",content:"cont",date:Date(),priority:0)
+    let deck = Deck(name:"deck1")
+    let todo = Todo(title:"1",content:"cont",date:Date(),priority:0,deck:deck)
+    deck.addTodo(todo:todo)
+    print(deck.getName())
     print(todo.getTitle())
+}
+public func sort(array:Array<Todo>,type:String) -> Array<Todo> {
+    // to sort array
+    // type can be : date,title,priority
+    return []
 }
 startMenu()
